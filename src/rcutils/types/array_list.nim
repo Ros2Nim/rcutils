c2e9c1e3a959a25c0b6ec5c078b33ae1538a6078
Jaremy Creechley <creechley@gmail.com>
2023-02-20 01:27:44 -0700

    fixup types

--- src/rcutils/types/array_list.nim
+++ src/rcutils/types/array_list.nim
@@ -1,34 +1,34 @@
 import rcutils_ret
 import ../allocator
-import ../allocator
 
-type array_list_impl_t* {.importc: "rcutils_array_list_impl_t", header: "array_list.h"} = object
-  size*: csize_t
-  capacity*: csize_t
-  list*: pointer
-  data_size*: csize_t
-  allocator*: allocator_t
-
-##  Copyright 2018-2019 Open Source Robotics Foundation, Inc.
-##
-##  Licensed under the Apache License, Version 2.0 (the "License");
-##  you may not use this file except in compliance with the License.
-##  You may obtain a copy of the License at
-##
-##      http://www.apache.org/licenses/LICENSE-2.0
-##
-##  Unless required by applicable law or agreed to in writing, software
-##  distributed under the License is distributed on an "AS IS" BASIS,
-##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-##  See the License for the specific language governing permissions and
-##  limitations under the License.
-##  \file
-
-discard "forward decl of rcutils_array_list_impl_t"
 type
 
+  rcutils_array_list_impl_t* {.importc: "rcutils_array_list_impl_t",
+                               header: "array_list.h", bycopy.} = object ##
+                              ##  Copyright 2018-2019 Open Source Robotics Foundation, Inc.
+                              ##
+                              ##  Licensed under the Apache License, Version 2.0 (the "License");
+                              ##  you may not use this file except in compliance with the License.
+                              ##  You may obtain a copy of the License at
+                              ##
+                              ##      http://www.apache.org/licenses/LICENSE-2.0
+                              ##
+                              ##  Unless required by applicable law or agreed to in writing, software
+                              ##  distributed under the License is distributed on an "AS IS" BASIS,
+                              ##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                              ##  See the License for the specific language governing permissions and
+                              ##  limitations under the License.
+                              ##  \file
+    size* {.importc: "size".}: csize_t
+    capacity* {.importc: "capacity".}: csize_t
+    list* {.importc: "list".}: pointer
+    data_size* {.importc: "data_size".}: csize_t
+    allocator* {.importc: "allocator".}: rcutils_allocator_t
+
+
   rcutils_array_list_t* {.importc: "rcutils_array_list_t",
-                          header: "array_list.h", bycopy.} = object
+                          header: "array_list.h", bycopy.} = object ##
+                              ##  The structure holding the metadata for an array list.
     impl* {.importc: "impl".}: ptr rcutils_array_list_impl_t ##
                               ##  A pointer to the PIMPL implementation type.
 
